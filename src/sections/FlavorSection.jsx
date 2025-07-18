4627802d88028ca84a98910d75a41b3db38bd7dc
tyujjwal6 <ujjwal.singh.csit.2022@miet.ac.in>
2025-07-18 14:40:07 +0530

    first commit

---
+++ src/sections/FlavorSection.jsx
@@ -0,0 +1,23 @@
+import FlavorSlider from "../components/FlavorSlider"
+import FlavorTitle from "../components/flavorTitle"
+
+const FlavorSection = () => {
+  return (
+    <section className="flavor-section">
+
+        <div className="h-full flex lg:flex-row flex-col items-center relative ">
+
+            <div className="lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0">
+                <FlavorTitle />
+            </div>
+            <div className="h-full">
+                <FlavorSlider />
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default FlavorSection
